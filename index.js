31d0c476744df3bc62aa66b3e17fc321eab05076
Ivan Smolyarov <i.a.smoliarov@gmail.com>
2023-02-28 17:03:13 +0300

    Setup project

---
+++ index.js
@@ -0,0 +1,19 @@
+/* eslint-disable import/no-extraneous-dependencies */
+require('dotenv').config();
+// Require the necessary discord.js classes
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+const token = process.env.TOKEN;
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, (c) => {
+  // eslint-disable-next-line no-console
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+// Log in to Discord with your client's token
+client.login(token);
